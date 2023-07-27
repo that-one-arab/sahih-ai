d1d3dddf5a485341a405b3f6038a88e1e983f140
Muhammed Aldulaimi <muhammed.aldulaimi98@gmail.com>
2023-07-27 10:14:38 +0300

    Add production environment handling to main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import os
 import quart
 import quart_cors
 from quart import request, jsonify
@@ -9,6 +10,10 @@ from lib.vectordb.query import query_docs
 from langchain.docstore.document import Document
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
+
+IS_PRODUCTION = os.getenv('PRODUCTION') == "True"
+PRODUCTION_URL = os.getenv('PRODUCTION_URL')
+PRODUCTION_EMAIL = os.getenv('PRODUCTION_CONTACT_EMAIL')
 
 def transform_documents_to_response(documents: list[Document]):
     new_list = []
@@ -36,8 +41,12 @@ def get_answer_template():
 {source}
 """
 
+@app.get('/hello')
+async def hello():
+    return jsonify(message="Hello, World!")
+
 @app.post("/api/query")
-async def add_todo():
+async def query_docs():
     request = await quart.request.get_json(force=True)
     query: str = request['query']
 
@@ -60,12 +69,17 @@ async def plugin_logo():
 async def plugin_manifest():
     with open("./static/openai/.well-known/ai-plugin.json") as f:
         text = f.read()
+        if IS_PRODUCTION:
+            text = text.replace("http://localhost:8000", PRODUCTION_URL)
+            text = text.replace("test@test.com", PRODUCTION_EMAIL)
         return quart.Response(text, mimetype="text/json")
 
 @app.get("/openapi.yaml")
 async def openapi_spec():
     with open("./static/openai/openapi.yaml") as f:
         text = f.read()
+        if IS_PRODUCTION:
+            text = text.replace("http://localhost:8000", PRODUCTION_URL)
         return quart.Response(text, mimetype="text/yaml")
 
 @app.get("/legal_info")
@@ -75,8 +89,14 @@ async def legal_info():
         html = f.read()
         return quart.Response(html)
 
+def dev():
+    app.run(debug=True, host="0.0.0.0", port=8000)
+
 def main():
-    app.run(debug=True, host="0.0.0.0", port=5003)
+    app.run()
 
 if __name__ == "__main__":
-    main()
+    if not IS_PRODUCTION:
+        dev()
+    else:
+        main()
